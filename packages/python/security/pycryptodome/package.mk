5a899a9b5e1b5deecaa57e4094716167e734ecd5
Unknown <gelotus@gmail.com>
2018-04-23 11:47:44 +0200

    bump pycryptodome to 3.6.1

--- packages/python/security/pycryptodome/package.mk
+++ packages/python/security/pycryptodome/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="pycryptodome"
-PKG_VERSION="3.6.0"
-PKG_SHA256="c06398137f95539468f46197b46bbb193c1e07d51852ca194b1142f88a38c8be"
+PKG_VERSION="3.6.1"
+PKG_SHA256="15013007e393d0cc0e69f4329a47c4c8597b7f3d02c12c03f805405542f70c71"
 PKG_ARCH="any"
 PKG_LICENSE="BSD"
 PKG_SITE="https://pypi.python.org/pypi/pycryptodome"
