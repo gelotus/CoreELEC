e791e9416dbff3a654dc59cc21cec80dce916cd9
Unknown <gelotus@gmail.com>
2018-04-26 14:47:44 +0200

    bump libinput to 1.10.5

--- packages/wayland/libinput/package.mk
+++ packages/wayland/libinput/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="libinput"
-PKG_VERSION="1.6.3"
-PKG_SHA256="74da2798728e79c3aedfffa268d8fe811c3766ca3708309c0dc45502be137da1"
+PKG_VERSION="1.10.5"
+PKG_SHA256="cbb1c03b8fb4b1d3781db5c8c66cdc1e754c0354037bcf39b2d8d08710077b2e"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/libinput/"
@@ -28,10 +28,7 @@ PKG_SECTION="wayland"
 PKG_SHORTDESC="libinput is a library to handle input devices in Wayland compositors and to provide a generic X.Org input driver."
 PKG_LONGDESC="libinput is a library to handle input devices in Wayland compositors and to provide a generic X.Org input driver."
 
-PKG_CONFIGURE_OPTS_TARGET="--with-sysroot=$SYSROOT_PREFIX \
-                           --enable-shared \
-                           --disable-static \
-                           --disable-documentation \
-                           --disable-event-gui \
-                           --disable-tests \
-                           --disable-libwacom"
+PKG_MESON_OPTS_TARGET="-Dlibwacom=false \
+                       -Ddebug-gui=false \
+                       -Dtests=false \
+                       -Ddocumentation=false"
