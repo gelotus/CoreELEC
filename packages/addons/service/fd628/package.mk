3acda73865f27294bdf2dd35cf4fcd5251fa4ec7
Adam Green <greena88@gmail.com>
2018-05-01 00:59:53 +0100

    fd628: mark package as broken as it is being replaced

--- packages/addons/service/fd628/package.mk
+++ packages/addons/service/fd628/package.mk
@@ -17,29 +17,24 @@
 ################################################################################
 
 PKG_NAME="fd628"
-PKG_VERSION="1.1"
-PKG_REV="101"
+PKG_VERSION="1.0"
+PKG_REV="100"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="https://libreelec.tv"
+PKG_SITE=""
 PKG_URL=""
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="service"
-PKG_SHORTDESC="fd628: Kodi service to light up additional icons on devices with FD628 display"
-PKG_LONGDESC="fd628: Kodi service to light up additional icons on devices with FD628 display"
+PKG_SHORTDESC="Add-on removed"
+PKG_LONGDESC="Add-on removed"
 PKG_TOOLCHAIN="manual"
 
-PKG_IS_ADDON="yes"
-PKG_ADDON_NAME="service.fd628"
-PKG_ADDON_PROJECTS="S905 S912"
-PKG_ADDON_TYPE="xbmc.service"
+PKG_ADDON_BROKEN="FD628 Display is no longer maintained and has been superseded by VFD Display."
 
-make_target() {
-  $SED -e "s|@PKG_VERSION@|$PKG_VERSION|g" \
-       -i addon.xml
-}
+PKG_IS_ADDON="yes"
+PKG_ADDON_NAME="FD628 Display"
+PKG_ADDON_TYPE="xbmc.broken"
 
 addon() {
-  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID
-  cp -R $PKG_BUILD/* $ADDON_BUILD/$PKG_ADDON_ID
+  :
 }
