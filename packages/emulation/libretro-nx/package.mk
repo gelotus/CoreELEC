ab5baa38a6e593ae0a28b5cdff549b6ac2f5602b
Unknown <gelotus@gmail.com>
2018-05-09 19:52:12 +0200

    Bump libretro-nx to 32d68ce

--- packages/emulation/libretro-nx/package.mk
+++ packages/emulation/libretro-nx/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="libretro-nx"
-PKG_VERSION="0d935ba"
-PKG_SHA256="dd71342ab3313b7ce8c0b1d2df4042099b62fcbe116c6efa30186bfe94219bb0"
+PKG_VERSION="32d68ce"
+PKG_SHA256="12da47c2a148cd18c33073877cdb73341d177e7f56f4d88300bd6640b4cb4b02"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/nxengine-libretro"
