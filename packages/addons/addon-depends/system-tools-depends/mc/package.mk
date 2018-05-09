7aca8e2f8c2adef516367fe66fbc73bf245ab555
Unknown <gelotus@gmail.com>
2018-05-09 20:23:48 +0200

    Bump mc to 4.8.20

--- packages/addons/addon-depends/system-tools-depends/mc/package.mk
+++ packages/addons/addon-depends/system-tools-depends/mc/package.mk
@@ -18,8 +18,8 @@
 ################################################################################
 
 PKG_NAME="mc"
-PKG_VERSION="4.8.19"
-PKG_SHA256="eb9e56bbb5b2893601d100d0e0293983049b302c5ab61bfb544ad0ee2cc1f2df"
+PKG_VERSION="4.8.20"
+PKG_SHA256="017ee7f4f8ae420a04f4d6fcebaabe5b494661075c75442c76e9c8b1923d501c"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.midnight-commander.org"
