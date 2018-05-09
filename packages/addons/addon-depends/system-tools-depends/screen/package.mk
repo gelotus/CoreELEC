3da5f803c12d5a9836233db1b4355ad5219cc8b7
Unknown <gelotus@gmail.com>
2018-05-09 23:00:06 +0200

    Bump screen to 4.6.2

--- packages/addons/addon-depends/system-tools-depends/screen/package.mk
+++ packages/addons/addon-depends/system-tools-depends/screen/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="screen"
-PKG_VERSION="4.5.1"
-PKG_SHA256="97db2114dd963b016cd4ded34831955dcbe3251e5eee45ac2606e67e9f097b2d"
+PKG_VERSION="4.6.2"
+PKG_SHA256="1b6922520e6a0ce5e28768d620b0f640a6631397f95ccb043b70b91bb503fa3a"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/screen/"
