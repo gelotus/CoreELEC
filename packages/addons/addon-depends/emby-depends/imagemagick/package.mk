7f06880492b2a63216184655b8f43802028ec1b9
Unknown <gelotus@gmail.com>
2018-05-09 20:13:50 +0200

    Bump imagemagick to 7.0.7-29

--- packages/addons/addon-depends/emby-depends/imagemagick/package.mk
+++ packages/addons/addon-depends/emby-depends/imagemagick/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="imagemagick"
-PKG_VERSION="7.0.7-1"
-PKG_SHA256="5a45e29509dbb23793a9c8db5c47ef1114c1ee82c9ca60053eaf06b3fc243e2c"
+PKG_VERSION="7.0.7-29"
+PKG_SHA256="be171d13e58bb19685b36080edf9d36b9b95633b0331fbf7e70b5997a3501d65"
 PKG_ARCH="any"
 PKG_LICENSE="http://www.imagemagick.org/script/license.php"
 PKG_SITE="http://www.imagemagick.org/"
