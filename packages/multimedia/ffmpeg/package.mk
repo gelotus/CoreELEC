955114fdfe834ed455f02be99cb0365cec9a825b
Unknown <gelotus@gmail.com>
2018-06-10 16:10:49 +0200

    ffm

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -81,9 +81,9 @@ else
 fi
 
 if [ "$TARGET_ARCH" = "x86_64" ]; then
-  PKG_FFMPEG_X86ASM="--enable-x86asm"
+  FFMPEG_X86ASM="--enable-x86asm"
 else
-  PKG_FFMPEG_X86ASM="--disable-x86asm"
+  FFMPEG_X86ASM="--disable-x86asm"
 fi
 
 pre_configure_target() {
@@ -205,8 +205,8 @@ configure_target() {
               --enable-zlib \
               --enable-asm \
               --disable-altivec \
-              $PKG_FFMPEG_FPU \
-              $PKG_FFMPEG_X86ASM \
+              $FFMPEG_FPU \
+              $FFMPEG_X86ASM \
               --disable-symver
 }
 
