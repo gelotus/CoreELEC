d382b4bbfbafc4beedc1cb048d5f61b0b8981c41
Unknown <gelotus@gmail.com>
2018-06-12 21:27:30 +0200

    Merge branch 'master' into testing

--- packages/sysutils/amremote/package.mk
+++ packages/sysutils/amremote/package.mk
@@ -16,29 +16,29 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="brcmfmac_sdio-firmware-rpi"
-PKG_VERSION="b530731"
-PKG_SHA256="c219f1d232fb80fbf23e7f2ed97d9c495855355a36dda373a8985c699ee1dd4b"
-PKG_ARCH="any"
-PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/LibreELEC/LibreELEC.tv"
-PKG_URL="https://github.com/LibreELEC/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain"
-PKG_SECTION="firmware"
-PKG_SHORTDESC="brcmfmac_sdio-firmware: firmware for brcm bluetooth chips used on RaspberryPi devices"
-PKG_LONGDESC="Firmware for Broadcom Bluetooth chips used on RaspberryPi devices, and brcm-patchram-plus that downloads a patchram files in the HCD format to the Bluetooth based silicon and combo chips and other utility functions."
-PKG_TOOLCHAIN="manual"
+PKG_NAME="amremote"
+PKG_VERSION="6431040"
+PKG_SHA256="5859680b0951ed3d2265999b7ad5309060587815df4dd1c48c6fa9aae039d5c5"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="other"
+PKG_SITE="http://www.amlogic.com"
+PKG_URL="https://github.com/codesnake/amremote/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain usbutils"
+PKG_SECTION="sysutils/remote"
+PKG_SHORTDESC="amremote - IR remote configuration utility for Amlogic-based devices"
+PKG_LONGDESC="amremote - IR remote configuration utility for Amlogic-based devices"
 
 makeinstall_target() {
-  DESTDIR=$INSTALL/$(get_kernel_overlay_dir) ./install
-}
-
-post_makeinstall_target() {
-  # Install rpi btuart script to bring up Bluetooth
   mkdir -p $INSTALL/usr/bin
-    cp -P $PKG_DIR/scripts/rpi-btuart $INSTALL/usr/bin
+    cp remotecfg $INSTALL/usr/bin
+
+  mkdir -p $INSTALL/usr/lib/coreelec
+    cp $PKG_DIR/scripts/* $INSTALL/usr/lib/coreelec
+
+  mkdir -p $INSTALL/etc/amremote
+    cp $PKG_DIR/config/*.conf $INSTALL/etc/amremote
 }
 
 post_install() {
-  enable_service brcmfmac_sdio-firmware.service
+  enable_service amlogic-remotecfg.service
 }
